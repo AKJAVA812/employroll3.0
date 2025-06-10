f5c82e51ae35010fe6f94d9bf6bedf457d46af9b
bharat-rajora12 <bharat.rajora@employroll.in>
2025-06-10 10:47:10 +0530

    m

--- lib/commanScreen/allAPIList.dart
+++ lib/commanScreen/allAPIList.dart
@@ -4,7 +4,7 @@ class ApiDetails{
    //static var server="http://www.employroll.com/";
    static var serverTwo="http://super.employroll.com:8081/";
    //static var serverTwo="https://14368a81b89e.ngrok.app/";
-   static var server="https://0a4e17689bb7.ngrok.app/";
+   static var server="https://8bd28be0508e.ngrok.app/";
    //Attendance Punch in and Punch out
    static String login="/restful/service/login";
    static String punchIn="/restful/service/attendance/via/mobile";
