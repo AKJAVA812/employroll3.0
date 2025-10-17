368f58feed1e14ab929bd9912dc3858f1cc5d356
bharat-rajora12 <bharat.rajora@employroll.in>
2025-10-17 10:27:59 +0530

    m

--- lib/commanScreen/punchInOutScreen.dart
+++ lib/commanScreen/punchInOutScreen.dart
@@ -1286,7 +1286,7 @@ class _DefaultPageState extends State<DefaultPage> {
           title: const Text("Select Your Location"),
           content: const Text("No geofence data available"),
           actions: [
-            TextButton(onPressed: () => Navigator.pop(context), child: const Text("OK"))
+            TextButton(onPressed: () => Navigator.of(context, rootNavigator: true).pop(), child: const Text("OK"))
           ],
         ),
       );
@@ -1422,7 +1422,7 @@ class _DefaultPageState extends State<DefaultPage> {
           title: const Text("Select Your Location"),
           content: const Text("No geofence data available"),
           actions: [
-            TextButton(onPressed: () => Navigator.pop(context), child: const Text("OK"))
+            TextButton(onPressed: () => Navigator.of(context, rootNavigator: true).pop(), child: const Text("OK"))
           ],
         ),
       );
